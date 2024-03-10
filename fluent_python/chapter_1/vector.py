61e52e9b5297fb944294625bbd2e22bc9bf6f861
Bilski311 <dominik.bilski24@gmail.com>
2024-03-10 19:50:57 +0100

    Pythonic vector.

---
+++ fluent_python/chapter_1/vector.py
@@ -0,0 +1,29 @@
+import math
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f'Vector({self.x!r}, {self.y!r})'
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __mul__(self, other):
+        return Vector(self.x * other, self.y * other)
+
+    def __abs__(self):
+        return math.hypot(self.x, self.y)
+    def __bool__(self):
+        return bool(abs(self))
+
+
+vector = Vector(2, 1)
+vector2 = Vector(2, 2)
+print(vector)
+print(vector2)
+print(vector + vector2)
+print(vector * 3)
+print(bool(vector))
+print(bool(Vector(0, 0)))
