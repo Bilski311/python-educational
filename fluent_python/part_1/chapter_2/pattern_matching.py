8f0eba2277626b6b14e45710234984b52987a20e
Bilski311 <dominik.bilski24@gmail.com>
2024-03-12 23:36:47 +0100

    Expand pattern matching example.

--- fluent_python/part_1/chapter_2/pattern_matching.py
+++ fluent_python/part_1/chapter_2/pattern_matching.py
@@ -3,6 +3,8 @@ metro_areas = [
     ('Delhi NCR', 'IN', 21.935, (28.613889, 77.208889)),
     ('Mexico City', 'MX', 20.142, (19.433333, -99.133333)),
     ('New York-Newark', 'US', 20.104, (40.808611, -74.020386)),
+    ('Washington DC', 'US'),
+    ('Baltimore', 'US'),
     ('São Paulo', 'BR', 19.649, (-23.547778, -46.635833)),
 ]
 
@@ -12,3 +14,5 @@ for record in metro_areas:
     match record:
         case [city, _, _, (lat, lon)] if lon < 0:
             print(f'{city:15} | {lat:9.4f} | {lon:9.4f}')
+        case [city, 'US']:
+            print(f'{city} is a US city with undisclosed location')
